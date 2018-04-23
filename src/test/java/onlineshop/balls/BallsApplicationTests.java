4d2a2a88daeada4aa2bfddaf6aff55ca400fac3f
MichalakSzymon <szymon.michalak@vp.pl>
2018-04-23 16:39:43 +0200

    Online  Balls Shop

---
+++ src/test/java/onlineshop/balls/BallsApplicationTests.java
@@ -0,0 +1,16 @@
+package onlineshop.balls;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BallsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
